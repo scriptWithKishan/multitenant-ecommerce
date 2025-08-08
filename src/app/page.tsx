b7f48ce68f6dc3d45ce61755d2e0c11b76a9c2cd
scriptWithKishan <kishan.2785.2003@gmail.com>
2025-08-08 20:38:12 +0000

    Custom styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p>
-        
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an Input " />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="This is textarea" />
+        </div>
+      </div>
     </div>
   );
 }
